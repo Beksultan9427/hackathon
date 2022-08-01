927ea45ac081f506ad09a7c89ce0379073395cd4
Beksultan <beksultan9427@gmail.com>
2022-08-01 19:34:14 +0600

    Добавил Navbar

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Navbar from "./components/Navbar";
 function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default Navigation;
