16ae4f97fbf87dbedee5b6573dfbf24caa73d8c4
Ariet <105222083+ariet587@users.noreply.github.com>
2022-08-01 21:18:40 +0600

    Изменение провайдеры

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -4,8 +4,10 @@ import MainPage from "./pages/MainPage";
 import Navbar from "./components/Navbar";
 import AdminAddPage from "./pages/AdminAddPage";
 import AdminPage from "./pages/AdminPage";
-import AdminProvider from "./Context/AdminProvider";
-import ClientProvider from "./Context/ClientProvider";
+import AdminEditPage from "./pages/AdminEditPage";
+import AdminProvider from "./contexts/AdminProvider";
+import ClientProvider from "./contexts/ClientProvider";
+
 function Navigation() {
   return (
     <div>
@@ -17,6 +19,7 @@ function Navigation() {
               <Route path="/" element={<MainPage />} />
               <Route path="/admin/add" element={<AdminAddPage />} />
               <Route path="/admin/" element={<AdminPage />} />
+              <Route path="/admin/edit/:id" element={<AdminEditPage />} />
             </Routes>
           </BrowserRouter>
         </AdminProvider>
