c6cf613bb1e1e3207f274b4a983f2749cbfcc03a
Ariet <105222083+ariet587@users.noreply.github.com>
2022-08-01 20:32:52 +0600

    ПРовайдеры

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -4,17 +4,23 @@ import MainPage from "./pages/MainPage";
 import Navbar from "./components/Navbar";
 import AdminAddPage from "./pages/AdminAddPage";
 import AdminPage from "./pages/AdminPage";
+import AdminProvider from "./Context/AdminProvider";
+import ClientProvider from "./Context/ClientProvider";
 function Navigation() {
   return (
     <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/admin/add" element={<AdminAddPage />} />
-          <Route path="/admin/" element={<AdminPage />} />
-        </Routes>
-      </BrowserRouter>
+      <ClientProvider>
+        <AdminProvider>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/admin/add" element={<AdminAddPage />} />
+              <Route path="/admin/" element={<AdminPage />} />
+            </Routes>
+          </BrowserRouter>
+        </AdminProvider>
+      </ClientProvider>
     </div>
   );
 }
