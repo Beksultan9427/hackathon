946776cc7627f5c82ad1effce1c4df01960e1064
Beksultan <beksultan9427@gmail.com>
2022-08-01 19:55:03 +0600

    Добавил MacPage

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Navbar from "./components/Navbar";
+import MacPage from "./pages/MacPage";
 function Navigation() {
   return (
     <div>
@@ -9,6 +10,7 @@ function Navigation() {
         <Navbar />
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/mac" element={<MacPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/MacPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MacPage() {
+  return <div>MacPage</div>;
+}
+
+export default MacPage;
