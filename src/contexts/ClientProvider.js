fdf5feab05c01243cc96aa8f6dc63a7a4f6b6d39
Ariet <105222083+ariet587@users.noreply.github.com>
2022-08-02 21:15:04 +0600

    pull

--- src/contexts/ClientProvider.js
+++ src/contexts/ClientProvider.js
@@ -6,7 +6,7 @@ const reducer = (state, action) => {
   if (action.type === "GET_PHONS") {
     return {
       ...state,
-      watches: action.payload,
+      phones: action.payload,
     };
   }
 
